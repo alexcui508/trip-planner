2c55095a02c33b543e0d4528a5dcd8fcd49b2933
Alex Cui <alexcui@Alexs-MacBook-Pro.local>
2019-04-29 07:28:01 -0700

    businessdetail update

--- src/components/BusinessDetail.jsx
+++ src/components/BusinessDetail.jsx
@@ -1,40 +1,33 @@
 import React from 'react';
-import Search from './Search';
-
-let REACT_APP_YELP_API_KEY = '1oVKZrEwzseUCh8sk57ORW4vBLSPDpk7tMDgrLSzsBVQ554QHwHKoghKGRNLhiVTajkH3zD8vuxepiWhu9GMCwglYwZqUiYlGrR9__0DMsVxEuTyL1iITpqI8qHGXHYx';
+import { Grid, Modal } from 'semantic-ui-react';
 
 class BusinessDetail extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: null,
-      details: [],
-    };
-  }
-
-  yelp_detailed_url(id) {
-    return "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id;
-  }
-
-  searchHandler(event) {
-    fetch(this.yelp_detailed_url(this.state.id), {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        Authorization: 'Bearer ' + REACT_APP_YELP_API_KEY
-      }
-    }).then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState({
-          details: data,
-        });
-      });
-  }
-
+  let renderedPhotos = (this.props.photos).map(photo => {<Modal trigger={photo}></Modal>});
   render() {
-
+    <Grid columns={3} divided>
+      <Grid.Row>
+        <Grid.Column>
+          <Modal trigger={this.props.photos}>
+            <Modal.Header>Select a Photo</Modal.Header>
+            <Modal.Content image>
+              <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
+              <Modal.Description>
+                <Header>Default Profile Image</Header>
+                <p>We've found the following gravatar image associated with your e-mail address.</p>
+                <p>Is it okay to use this photo?</p>
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
+          <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   }
 }
 
